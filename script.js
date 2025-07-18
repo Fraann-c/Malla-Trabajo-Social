d3f6d63c291a8614e62415eff385cfb6960426e9
Fraann-c <francisca.concha1120@gmail.com>
2025-07-18 02:58:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -36,6 +36,43 @@ const malla = {
     { id: "r30", nombre: "Trabajo Social con Colectivos II", prerequisitos: ["r22"] },
     { id: "r31", nombre: "Proyectos Sociales II" },
     { id: "r32", nombre: "Trabajo Social y Políticas Públicas" }
+  ],
+  "5° Semestre": [
+    { id: "r33", nombre: "Investigación Social Cuantitativa", prerequisitos: ["r26"] },
+    { id: "r34", nombre: "Investigación Social Cualitativa", prerequisitos: ["r26"] },
+    { id: "r35", nombre: "Taller Integrado de Intervención I", prerequisitos: ["r22"] },
+    { id: "r36", nombre: "Taller de Sistematización para la Intervención Social" },
+    { id: "r37", nombre: "Electivo de Desarrollo Personal" }
+  ],
+  "6° Semestre": [
+    { id: "r38", nombre: "Análisis de Datos Cuantitativos", prerequisitos: ["r33"] },
+    { id: "r39", nombre: "Análisis de Datos Cualitativos", prerequisitos: ["r34"] },
+    { id: "r40", nombre: "Taller Integrado de Intervención II", prerequisitos: ["r35"] },
+    { id: "r41", nombre: "Ética y Globalización" }
+  ],
+  "7° Semestre": [
+    { id: "r42", nombre: "Seminario de Investigación", prerequisitos: ["r38", "r39", "r40"] },
+    { id: "r43", nombre: "Nuevos Escenarios de Trabajo Social" },
+    { id: "r44", nombre: "Gestión Social para el Desarrollo" },
+    { id: "r45", nombre: "Electivo de Ética" }
+  ],
+  "8° Semestre": [
+    { id: "r46", nombre: "Seminario de Investigación e Intervención", prerequisitos: ["r42"] },
+    { id: "r47", nombre: "Gestión para la Intervención Social" },
+    { id: "r48", nombre: "Innovación Social y Desarrollo" },
+    { id: "r49", nombre: "Electivo de Responsabilidad Social" }
+  ],
+  "9° Semestre": [
+    { id: "r50", nombre: "Intervención Integrada I", prerequisitos: ["r46"] },
+    { id: "r51", nombre: "Gestión Pública" },
+    { id: "r52", nombre: "Optativo de Profundización I" },
+    { id: "r53", nombre: "Optativo de Profundización II" }
+  ],
+  "10° Semestre": [
+    { id: "r54", nombre: "Intervención Integrada II", prerequisitos: ["r50"] },
+    { id: "r55", nombre: "Gestión de Políticas Sectoriales" },
+    { id: "r56", nombre: "Optativo de Profundización III" },
+    { id: "r57", nombre: "Optativo de Profundización IV" }
   ]
 };
 
